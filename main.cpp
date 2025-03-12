28aa5c277a1cfaa6803fc1eb5fb42938651d40c0
njwmerv <montoya.nicanor04@gmail.com>
2025-03-12 01:37:30 -0400

    Added colouring for the Perlin noise to look like terrain

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <SFML/Graphics.hpp>
 #include "perlin.h"
 #include "SFML/Graphics/Image.hpp"
 #include "SFML/Graphics/RenderTexture.hpp"
@@ -6,6 +8,27 @@
 #include "SFML/Graphics/Texture.hpp"
 #include "SFML/Window/VideoMode.hpp"
 using namespace std;
+
+sf::Color simpleTerrainColor(const float elevation) {
+	if(elevation < 0.3f){ // Deep water
+		return sf::Color(0, 0, 150);
+	}
+	else if(elevation < 0.4f){ // Shallow water
+		return sf::Color(0, 100, 200);
+	}
+	else if(elevation < 0.425f){ // Beach
+		return sf::Color(240, 220, 130);
+	}
+	else if(elevation < 0.7f){ // Grass
+		return sf::Color(34, 139, 34);
+	}
+	else if(elevation < 0.8f){ // Mountain
+		return sf::Color(100, 100, 100);
+	}
+	else{ // Snow
+		return sf::Color(255, 255, 255);
+	}
+}
 
 int main(){
 	const int WINDOW_WIDTH = 1920;
@@ -24,7 +47,7 @@ int main(){
 
 	// Generating the Perlin noise
 	const int seed = static_cast<int>(time(0));
-	Perlin noiseGenerator = Perlin(seed, 255, 255);
+	Perlin noiseGenerator = Perlin(seed);
 
 	for(unsigned row = 0; row < WINDOW_HEIGHT; row++){
 		for(unsigned col = 0; col < WINDOW_WIDTH; col++){
@@ -37,8 +60,7 @@ int main(){
 				frequency *= 2.0;
 			}
 			noise = (noise + 1.0) / 2.0; // transform from [-1, 1] -> [0, 1]
-			const std::uint8_t intensity = static_cast<std::uint8_t>(noise * 255);
-			noiseImage.setPixel({col, row}, sf::Color(intensity, intensity, intensity));
+			noiseImage.setPixel({col, row}, simpleTerrainColor(noise));
 		}
 	}
 
